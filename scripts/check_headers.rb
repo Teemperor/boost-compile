46e3f6617def878eced5c6fd39b61ce062464c13
yuka <yukatkh@gmail.com>
2017-11-19 17:55:19 +0900

    Modifed check_headers

--- scripts/check_headers.rb
+++ scripts/check_headers.rb
@@ -4,8 +4,8 @@ File.open(ARGV[0], "r").read.each_line do |line|
   prefix = "boost-compile/"
   if line[0,5].count('.') <= 2 and line =~ %r(#{prefix}(.*?)$)
     name = $1
-    if name.match(/#ifdef|#ifndef/) == nil
-      p name
+    if /ifn?def/ !~ File.read(name)
+      print name + " "
     end
   end
 end
